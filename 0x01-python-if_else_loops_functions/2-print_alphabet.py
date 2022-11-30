365d027478784a27cb2705916354dbb06d220884
maiyo008 <tonymaiyo008@gmail.com>
2022-11-30 02:10:29 +0000

    Output correction and pycode style correction

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for l in range(97, 123):
-    if chr(l) != 'q' and chr(l) != 'e':
-        print("{}".format(chr(l)), end="")
+for alphabet in range(97, 123):
+    print("{}".format(chr(alphabet)), end="")

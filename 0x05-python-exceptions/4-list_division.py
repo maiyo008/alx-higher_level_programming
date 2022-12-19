3298d8e15c7332c10a226c219236bcf3c62d6697
maiyo008 <tonymaiyo008@gmail.com>
2022-12-19 16:55:27 +0000

    Fix output exception messages to match ones provided

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,13 +17,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             quotient = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("Wrong type")
+            print("wrong type")
             quotient = 0
         except ZeroDivisionError:
             print("division by 0")
             quotient = 0
         except IndexError:
-            print("Out of range")
+            print("out of range")
             quotient = 0
         finally:
             quotient_list.append(quotient)

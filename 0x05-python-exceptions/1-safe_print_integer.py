659db23bb659d5b7d9a03a490a68814e3053b733
maiyo008 <tonymaiyo008@gmail.com>
2022-12-19 15:09:43 +0000

    Pycode style correction

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,15 @@
 #1-safe_print_integer.py
 
 def safe_print_integer(value):
+    """A function that prints an integer 
+
+    args:
+        value : an input value of any type
+
+    return:
+        True if value has been printed successfully
+        False if otherwise
+        """
     try:
         print("{:d}".format(value))
         return (True)
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -21,4 +21,5 @@
 |--------|------|
 |Task 0  |Write a function that prints `x` elements of a list|
 |Task 1  |Write a function that prints an integer with `{:d}".format()`|
+|Task 2  |Write a function that prints the first `x` elements of a list and only intergers|
 

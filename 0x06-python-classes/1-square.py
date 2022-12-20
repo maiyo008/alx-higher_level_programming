b3281d001c9b426d6c8f1092ca5eca5c5995a516
maiyo008 <tonymaiyo008@gmail.com>
2022-12-20 20:01:26 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-square.py
+# Tony Maiyo
+
+"""Define the class Square"""
+
+class Square:
+    """Initializes a new square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+
+        args:
+            size(int): The size of the new square.
+        """
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -34,4 +34,5 @@
 |Tasks no |Tasks	|
 |---------|-------------|
 |Task 0   |Write an empty class Square that defines a square|
+|Task 1   |Write a class `Square` that defines a square by:(based on `0-square.py`. Private instance attribute: `size`|
 

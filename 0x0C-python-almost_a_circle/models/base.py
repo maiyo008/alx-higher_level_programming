419dcac36de727c81b9dae720146e30e6d96c0f6
maiyo008 <tonymaiyo008@gmail.com>
2023-01-16 20:22:55 +0000

    task 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,16 +57,34 @@ class Base():
             return []
         return json.loads(json_string)
     @classmethod
-        def create(cls, **dictionary):
-            """Return a class instantied from a dictionary of attributes.
+    def create(cls, **dictionary):
+    """Return a class instantied from a dictionary of attributes.
+        
+    Args:
+        **dictionary (dict): Key/value pairs of attributes to initialize.
+    """
+    if dictionary and dictionary != {}:
+        if cls.__name__ == "Rectangle":
+            new = cls(1, 1)
+        else:
+            new = cls(1)
+        new.update(**dictionary)
+        return new
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file of JSON strings.
 
-            Args:
-                **dictionary (dict): Key/value pairs of attributes to initialize.
-            """
-            if dictionary and dictionary != {}:
-                if cls.__name__ == "Rectangle":
-                    new = cls(1, 1)
-                else:
-                    new = cls(1)
-                new.update(**dictionary)
-                return new
+        Reads from `<cls.__name__>.json`.
+
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+

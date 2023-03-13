742aeb4eff65ece1270bdaaeb1ec2aec72759656
maiyo008 <tonymaiyo008@gmail.com>
2023-03-13 21:26:25 +0000

    Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2], 10);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let line = '';
+    for (let j = 0; j < size; j++) {
+      line += 'X';
+    }
+    console.log(line);
+  }
+}

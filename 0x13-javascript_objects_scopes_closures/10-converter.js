bce095f2e16dab2a3373d2524dca78544a514559
maiyo008 <tonymaiyo008@gmail.com>
2023-03-14 21:43:41 +0000

    Task 6 correction

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 exports.converter = function converter (base) {
   return x => x.toString(base);
-}
+};
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,4 +1,4 @@
-#!/sr/bin/node
+#!/usr/bin/node
 const square = require('./5-square');
 class Square extends square {
   charPrint (c) {

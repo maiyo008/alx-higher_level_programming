7bb9f84f02ea8fbc9ee4110e0befb5653f356f9f
maiyo008 <tonymaiyo008@gmail.com>
2023-03-17 20:12:51 +0000

    Do task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This script takes in an argument and displays all the values in the states
+table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+import sys
+
+
+def my_filter_states():
+    """
+    This function shows states that match the state provided as an input arg
+    """
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+    query_name = sys.argv[4]
+    db = MySQLdb.connect(
+        user=mysql_user,
+        password=mysql_password,
+        database=mysql_db)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states WHERE name = %s""", (query_name,))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
+
+
+if __name__ == "__main__":
+    my_filter_states()

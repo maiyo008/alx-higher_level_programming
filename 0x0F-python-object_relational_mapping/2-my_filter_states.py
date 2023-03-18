22695787ca06eda686c2f1f4fdef5429b6af595c
maiyo008 <tonymaiyo008@gmail.com>
2023-03-18 09:49:44 +0000

    Correct task 2 to use format for query input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,8 +19,9 @@ def my_filter_states():
         password=mysql_password,
         database=mysql_db)
     c = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s"
-    c.execute(query, (query_name,))
+    # Execute query
+    query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
+    c.execute(query, (query_name + "%",))
     rows = c.fetchall()
     for row in rows:
         print(row)

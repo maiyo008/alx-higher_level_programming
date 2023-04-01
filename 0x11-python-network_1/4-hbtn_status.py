db6c6b4e4489b6ad5f5f76034fb14c997593e973
maiyo008 <tonymaiyo008@gmail.com>
2023-04-01 02:35:47 +0000

    fetch url and display body

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+    print("Body Response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
